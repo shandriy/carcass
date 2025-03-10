a951e08874d76e586956dd3a3de5cc82dfeb5605
shandriy <151408521+shandriy@users.noreply.github.com>
2025-03-09 23:39:37 -0700

    Remove duplicate words

--- index.sh
+++ index.sh
@@ -18,12 +18,17 @@ while read url; do
   length_1=${#url_contents}
   length_2=$((${#url_contents} + 1))
 
+  touch "index/1/$url"
+
   while [ "$length_1" -lt "$length_2" ]; do
     token="${url_contents%%[^A-Za-z0-9]*}"
     token=$(printf %s "$token" | tr "[:upper:]" "[:lower:]")
 
     if [ "$token" != "" ]; then
-      printf "%s\n" "$token" >> "index/1/$url"
+      contains_word=$(grep -xF "$token" "index/1/$url")
+      if [ -z "$contains_word" ]; then
+        printf "%s\n" "$token" >> "index/1/$url"
+      fi
     fi
 
     length_2=${#url_contents}
