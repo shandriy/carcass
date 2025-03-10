e84e05ce1b40322124bd1f4a3d4efe9e2f3d2dc0
shandriy <151408521+shandriy@users.noreply.github.com>
2025-03-10 15:11:37 -0700

    Add basic search function

---
+++ search.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+first=true
+
+rm results
+
+for arg; do
+  if [ -n $first ]; then
+    first=""
+
+    cp "index/3/$arg" results
+  else
+    while read line; do
+      if [ -n "$(grep -xF "$line" results)" ]; then
+        printf "%s\n" "$line" >> "_results"
+      fi
+    done < "index/3/$arg"
+
+    rm results
+    mv "_results" results
+  fi
+done
+
+mv results "_results"
+
+while read line; do
+  printf "%s\n" $(printf %s "$line" | base64 -d) >> results
+done < "_results"
+
+rm "_results"
